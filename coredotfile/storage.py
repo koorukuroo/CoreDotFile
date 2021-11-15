e5e97598d3bc539dda3924308cdc91d0dc34c0f4
koorukuroo <koorukuroo@gmail.com>
2021-11-15 14:29:52 +0900

    Add put_object_storage

--- coredotfile/storage.py
+++ coredotfile/storage.py
@@ -1,1 +1,20 @@
 """Main module."""
+import requests
+
+
+coredot_storage_url = "https://tool.core.today/v1/storage/object/url"
+
+
+def get_presigned_url(key):
+    r = requests.put(coredot_storage_url, params={"key": key})
+    result = r.json()
+    return result
+
+
+def put_object_storage(key, content):
+    if type(content) is not bytes:
+        return {'status': '500', 'message': 'Use bytes type content'}
+
+    info = get_presigned_url(key)
+    result = requests.put(info['url'], data=content)
+    return {'status': result['status_code'], 'message': 'Upload successful'}
