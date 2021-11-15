66fde26547b84716c6580ba9931e34987a911bdc
koorukuroo <koorukuroo@gmail.com>
2021-11-15 14:41:13 +0900

    Add get_object_storage

--- coredotfile/storage.py
+++ coredotfile/storage.py
@@ -5,8 +5,14 @@ import requests
 coredot_storage_url = "https://tool.core.today/v1/storage/object/url"
 
 
-def get_presigned_url(key):
+def get_presigned_upload_url(key):
     r = requests.put(coredot_storage_url, params={"key": key})
+    result = r.json()
+    return result
+
+
+def get_presigned_download_url(key):
+    r = requests.get(coredot_storage_url, params={"key": key})
     result = r.json()
     return result
 
@@ -15,6 +21,20 @@ def put_object_storage(key, content):
     if type(content) is not bytes:
         return {'status': '500', 'message': 'Use bytes type content'}
 
-    info = get_presigned_url(key)
+    info = get_presigned_upload_url(key)
     result = requests.put(info['url'], data=content)
     return {'status': result['status_code'], 'message': 'Upload successful'}
+
+
+def get_object_storage(key, target_file=None, filename=None):
+    if not filename:
+        filename = key
+    if not target_file:
+        target_file = key
+
+    info = get_presigned_download_url(key)
+    r = requests.get(info['url'])
+    with open(target_file, 'wb') as f:
+        f.write(r.content)
+    return {'status': r['status_code'], 'message': 'Download successful', 'content': r.content}
+
