48e344f03aea2b56df3d178059a658b0a5732fd8
David Kim <50807669+DavidKimDY@users.noreply.github.com>
2021-11-25 16:44:26 +0900

    Edit : verify False

--- coredotfile/download.py
+++ coredotfile/download.py
@@ -3,7 +3,7 @@ import requests
 
 def download_file(url: str, apikey: str, filename: str):
     # NOTE the stream=True parameter below
-    with requests.post(url, data={"apikey": apikey}, stream=True) as r:
+    with requests.post(url, data={"apikey": apikey}, stream=True, verify=False) as r:
         r.raise_for_status()
         with open(filename, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
