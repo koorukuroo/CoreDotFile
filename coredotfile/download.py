25fdde57ca3d5c68f8334f2f7cb704010fb77abe
DavidKimDY <keayoub@gmail.com>
2021-11-25 12:30:35 +0900

    Add : download file

---
+++ coredotfile/download.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def download_file(url: str, apikey: str, filename: str):
+    # NOTE the stream=True parameter below
+    with requests.post(url, data={"apikey": apikey}, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                # if chunk:
+                f.write(chunk)
+    return filename
